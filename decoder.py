7db56528cb526dc6cef22967865e50c9def4df45
masha-len-ub-g-i3 <david.marn@gmail.com>
2016-05-24 18:54:01 -0700

    Extracts needed fields from JSON dumps

--- decoder.py
+++ decoder.py
@@ -1,6 +1,29 @@
 import json
 
-print(json.dumps({'key1': 1, 'key2':2}))
-sample_file = open('test_data/10.20.32.40.feed','r')
-sample_file_json = json.load(sample_file)
-print(sample_file_json['core.resources']['connections'])
+class Decoder:
+    """
+    Extracts data from nodewatcher JSON dumps
+    """
+    def __init__(self, filename):
+        self.file = open(filename,'r')
+        self.json = json.load(self.file)
+
+    def getNodeModel(self):
+        return self.json["core.general"]["hardware"]["model"]
+
+    def getNodeChannel(self, interface="wlan0"):
+        return self.json["core.wireless"]["interfaces"][interface]["channel"]
+
+    def getNodeSNR(self, interface="wlan0"):
+        return self.json["core.wireless"]["interfaces"][interface]["signal"]-self.json["core.wireless"]["interfaces"][interface]["noise"]
+
+    def getNodeBSSID(self, interface="wlan0"):
+        return self.json["core.wireless"]["interfaces"][interface][bssid]
+
+    def getNodeNeighbors(self, radio="phy1"):
+        """
+        returns a dictionary of all access points in the vicinity along with the signal strength of each access point.
+        :param: radio used to survey surroundings
+        :return: a dictionary of neighbors
+        """
+        return self.json["core.wireless"]["radios"][radio]["survey"]
